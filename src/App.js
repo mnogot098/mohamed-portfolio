d8f338faede2b8924fa192e8aab8cb15f1d0b9d6
Mohamed <mnogot@orsys.co.ma>
2024-01-22 12:57:57 +0000

    sample change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
+import { Route, Routes } from "react-router-dom";
 
-import Homepage from "./pages/homepage";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Articles from "./pages/articles";
-import ReadArticle from "./pages/readArticle";
-import Contact from "./pages/contact";
 import Notfound from "./pages/404";
+import About from "./pages/about";
+import Articles from "./pages/articles";
+import Contact from "./pages/contact";
+import Homepage from "./pages/homepage";
+import Projects from "./pages/projects";
+import ReadArticle from "./pages/readArticle";
 
-import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
-
 	return (
 		<div className="App">
 			<Routes>
